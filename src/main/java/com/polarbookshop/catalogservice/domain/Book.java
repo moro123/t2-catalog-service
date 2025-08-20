137838893424eb31b94b0b071de22132a1f4a48e
Carlos <jcmamoro@gmail.com>
2025-08-20 14:48:00 -0600

    correcion de book and bookservice

--- src/main/java/com/polarbookshop/catalogservice/domain/Book.java
+++ src/main/java/com/polarbookshop/catalogservice/domain/Book.java
@@ -50,7 +50,7 @@ public record Book(
         int version
 ) {
         public static Book of(
-                String isbn, String title, String author, Double pricel, String publisher
+                String isbn, String title, String author, Double price, String publisher
         ) {
                 return new Book(
                         null, isbn, title, author, price, publisher, null, null, 0
